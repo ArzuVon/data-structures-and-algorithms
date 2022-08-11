897f5106af80cbe66a611e7ccbd04022cee54db1
Von <107226923+ArzuVon@users.noreply.github.com>
2022-08-11 00:14:32 -0400

    initial

--- stack-and-queue/challenge/src/queue.ts
+++ stack-and-queue/challenge/src/queue.ts
@@ -1,15 +1,21 @@
 export class Queue<T> {
-  get size(): number {
-    throw new Error("Not implemented");
-  }
 
-  get peek(): T {
-    throw new Error("Not implemented");
-  }
+storage: [] = [];
+get size(): number {
+  return this.storage.length;  
+}
 
-  enqueue(t: T): void {
-    throw new Error("Not implemented");
+get peek(): T {
+  if(this.isEmpty()){
+    throw new Error("Queue is empty");
+    }else{
+    return this.storage[this.storage.length -1];
   }
+}  }
+
+  enqueue(value: T): void {
+    this.storage.unshift(value);  
+}
 
   dequeue(): T {
     throw new Error("Not implemented");
