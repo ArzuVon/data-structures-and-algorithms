040941fe9f5e460a09f4f7239af4d631827cafaf
Von <107226923+ArzuVon@users.noreply.github.com>
2022-08-11 20:57:05 -0400

    finished Queue

--- stack-and-queue/challenge/src/queue.ts
+++ stack-and-queue/challenge/src/queue.ts
@@ -1,6 +1,7 @@
 export class Queue<T> {
 
-newArr: [] = [];
+newArr: T[] = [];
+
 get size(): number {
   return this.newArr.length;  
 }
@@ -8,16 +9,29 @@ get size(): number {
 get peek(): T {
   if(this.isEmpty()){
     throw new Error("Queue is empty");
-    }else{
+    } else {
     return this.newArr[this.newArr.length -1];
   }
-}  }
+}  
 
   enqueue(value: T): void {
     this.newArr.unshift(value);  
 }
 
-  dequeue(): T {
-    throw new Error("Not implemented");
+dequeue(): T | undefined {
+  if(this.isEmpty()) {
+    throw new Error( "Oueue is empty");
+  } else {
+  return this.newArr.shift();
+  }
+}
+
+isEmpty(): boolean {
+  if(this.newArr.length === 0){
+  return true;
+  } else {
+  return false;
+
+    }
   }
 }
