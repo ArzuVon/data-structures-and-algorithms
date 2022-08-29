aaac849de9f49aa3dd90d0f3b9187ea5cbbe6fc5
Von <107226923+ArzuVon@users.noreply.github.com>
2022-08-29 17:41:55 -0400

    Collections fixed

--- linked-list-insertions/src/Collection.ts
+++ linked-list-insertions/src/Collection.ts
@@ -1,7 +1,15 @@
+import { LinkedList } from "./LinkedList"
+
 export interface Collection<T> {
   insert(t: T): void;
   includes(t: T): boolean;
   toString(): string;
+  append(t:T): void;
+  insertBefore(t:T, value: T): void;
+  insertAfter(t: T, value: T): void;
+  kthFromEnd(t:number): any;
+  zip(ll1: LinkedList<T>, ll2: LinkedList<T>): LinkedList<T>;
+
 }
 
 export function display(t: any): string {
